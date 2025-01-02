62da9bea9155c02b964eeee459b96bec80d02f61
ComboTwTw <gfakemail18@gmail.com>
2025-01-02 23:12:28 +0500

    Update Navbar: add shrinking and background color change on scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import Home from "./Pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import { Scrollama, Step } from "react-scrollama";
+import { useState, useEffect } from "react";
 
 function App() {
+    const [scrollTrigger, setScrollTriger] = useState<boolean>(false);
+
+    const scrollCallback = () => {
+        scrollTrigger ? setScrollTriger(false) : setScrollTriger(true);
+    };
+
     return (
         <Router>
             <div className="flex flex-col items-center overflow-hidden w-full ">
-                <Navbar />
+                <Navbar scrollTrigger={scrollTrigger} />
+                <Scrollama offset={0} onStepEnter={scrollCallback}>
+                    <Step>
+                        <div className="min-w-[100px] min-h-[100px] bg-transparent absolute top-32 z-20"></div>
+                    </Step>
+                </Scrollama>
                 <Routes>
                     <Route path={"/"} element={<Home />} />
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
     navbar_logo,
     sm_menu_curl_left,
@@ -7,107 +7,128 @@ import {
 import { navLinks, heroText } from "../../constants";
 import { HamburgerSqueeze } from "react-animated-burgers";
 import { Fade } from "react-awesome-reveal";
-import { Scrollama, Step } from "react-scrollama";
 
-const Navbar = () => {
+interface Props {
+    scrollTrigger: boolean;
+}
+
+const Navbar = ({ scrollTrigger }: Props) => {
     const [toggleBurger, setToggleBurger] = useState<boolean>(false);
-    const [rectMoving, setRectMoving] = useState(false);
+
+    useEffect(() => {
+        console.log(scrollTrigger);
+    }, [scrollTrigger]);
 
     return (
         <nav
             className={`  ${
-                toggleBurger ? "bg-white md:bg-transparent" : "bg-transparent"
-            }  h-[70px] md:h-[100px] duration-500  w-full max-w-[1200px] fixed z-30`}
-            onScroll={() => {
-                setRectMoving(true);
-            }}
+                toggleBurger || scrollTrigger
+                    ? `bg-white ${!scrollTrigger && `md:bg-transparent`}`
+                    : "bg-transparent"
+            }    w-full  fixed z-50  flex justify-center duration-500`}
         >
-            <div className="z-[11] pt-[10px] pb-[10px] px-5 flex justify-between items-center">
-                {/* Logo */}
-                <Fade
-                    direction="up"
-                    damping={0.1}
-                    cascade={true}
-                    triggerOnce={true}
+            <div className="max-w-[1200px] w-full">
+                <div
+                    className={`z-[11] pt-[10px] pb-[10px] px-5 flex justify-between items-center `}
                 >
-                    <a href="#">
-                        <img
-                            src={navbar_logo}
-                            alt="Navbar Logo"
-                            className="max-h-[60px] md:max-h-[80px]"
+                    {/* Logo */}
+                    <Fade
+                        direction="up"
+                        damping={0.1}
+                        cascade={true}
+                        triggerOnce={true}
+                    >
+                        <a href="#">
+                            <img
+                                src={navbar_logo}
+                                alt="Navbar Logo"
+                                className={` ${
+                                    scrollTrigger
+                                        ? `max-h-[50px] md:max-h-[60px]`
+                                        : `max-h-[60px] md:max-h-[80px]`
+                                } duration-500`}
+                            />
+                        </a>
+                    </Fade>
+                    {/* Navlinks Desktop */}
+                    <Fade
+                        direction="up"
+                        damping={0.08}
+                        cascade={true}
+                        triggerOnce={true}
+                    >
+                        <ul className="hidden md:flex md:gap-8 lg:gap-[52px]">
+                            {navLinks.map((link) => {
+                                return (
+                                    <a
+                                        key={link.id}
+                                        href={link.linkTo}
+                                        className={`montserrat ${
+                                            scrollTrigger
+                                                ? `text-black duration-200`
+                                                : `text-white duration-200`
+                                        } hover:underline hover:decoration-mainRed duration-0  hover:decoration-[2px] hover:underline-offset-4`}
+                                    >
+                                        {link.tittle}
+                                    </a>
+                                );
+                            })}
+                        </ul>
+                    </Fade>
+                    {/* Hamburger Menu Mobile */}
+                    <div className="md:hidden ">
+                        <HamburgerSqueeze
+                            barColor={`${
+                                toggleBurger || scrollTrigger
+                                    ? "black"
+                                    : "white"
+                            }`}
+                            isActive={toggleBurger}
+                            buttonStyle={{
+                                padding: 0,
+                            }}
+                            buttonWidth={scrollTrigger ? 30 : 35}
+                            toggleButton={() => setToggleBurger(!toggleBurger)}
                         />
-                    </a>
-                </Fade>
-                {/* Navlinks Desktop */}
-                <Fade
-                    direction="up"
-                    damping={0.08}
-                    cascade={true}
-                    triggerOnce={true}
+                    </div>
+                </div>
+
+                {/* Navlinks Mobile */}
+                <div
+                    className={`md:hidden z-10 absolute right-0 left-0 ${
+                        toggleBurger ? "top-[70px]" : "top-[-755px]"
+                    } bg-white w-full flex flex-col items-center py-12 duration-500`}
                 >
-                    <ul className="hidden md:flex md:gap-8 lg:gap-[52px]">
+                    <ul className="flex flex-col items-center gap-12">
                         {navLinks.map((link) => {
                             return (
                                 <a
                                     key={link.id}
                                     href={link.linkTo}
-                                    className="montserrat text-white hover:underline hover:decoration-mainRed hover:udn hover:decoration-[2px] hover:underline-offset-4"
+                                    className="montserrat text-black text-2xl font-medium"
                                 >
                                     {link.tittle}
                                 </a>
                             );
                         })}
                     </ul>
-                </Fade>
-                {/* Hamburger Menu Mobile */}
-                <div className="md:hidden ">
-                    <HamburgerSqueeze
-                        barColor={`${toggleBurger ? "black" : "white"}`}
-                        isActive={toggleBurger}
-                        buttonStyle={{
-                            padding: 0,
-                        }}
-                        buttonWidth={35}
-                        toggleButton={() => setToggleBurger(!toggleBurger)}
+
+                    <button className="z-10 min-w-12 bg-mainRed mt-24 px-12 py-3 rounded-lg montserrat font-semibold text-white">
+                        {heroText.btn}
+                    </button>
+
+                    <img
+                        src={sm_menu_curl_left}
+                        alt="Mobile Menu curl at left"
+                        className="absolute left-0 top-28"
+                    />
+
+                    <img
+                        src={sm_menu_curl_right}
+                        alt="Mobile Menu curl at right"
+                        className="absolute right-0 top-64"
                     />
                 </div>
-            </div>
-
-            {/* Navlinks Mobile */}
-            <div
-                className={`md:hidden z-10 absolute right-0 left-0 ${
-                    toggleBurger ? "top-[70px]" : "top-[-755px]"
-                } bg-white w-full flex flex-col items-center py-12 duration-500`}
-            >
-                <ul className="flex flex-col items-center gap-12">
-                    {navLinks.map((link) => {
-                        return (
-                            <a
-                                key={link.id}
-                                href={link.linkTo}
-                                className="montserrat text-black text-2xl font-medium"
-                            >
-                                {link.tittle}
-                            </a>
-                        );
-                    })}
-                </ul>
-
-                <button className="z-10 min-w-12 bg-mainRed mt-24 px-12 py-3 rounded-lg montserrat font-semibold text-white">
-                    {heroText.btn}
-                </button>
-
-                <img
-                    src={sm_menu_curl_left}
-                    alt="Mobile Menu curl at left"
-                    className="absolute left-0 top-28"
-                />
-
-                <img
-                    src={sm_menu_curl_right}
-                    alt="Mobile Menu curl at right"
-                    className="absolute right-0 top-64"
-                />
             </div>
         </nav>
     );
